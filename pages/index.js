7994e7f0d972d2e703f9e3d7d3a9bd60a6142b77
Diego <diego_silvagomes@outlook.com>
2025-05-29 20:22:45 -0300

    Atuliza página Home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Márcia, eu te amo! Você é a melhor parte da minha vida! Te amarei para sempre!!! PS: O Luffy e o Creed também!!! ❤️ </h1>
+    return <h1>Home...</h1>
 }
 
 export default Home;
